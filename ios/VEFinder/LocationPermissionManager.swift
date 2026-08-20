c66f5dad8ab39d305d7e04449121df3c87af33e7
6yytn6brg6-ctrl <6yytn6brg6@privaterelay.appleid.com>
2026-08-20 20:49:24 +0200

    Add iOS location permission manager

---
+++ ios/VEFinder/LocationPermissionManager.swift
@@ -0,0 +1,15 @@
+import CoreLocation
+
+final class LocationPermissionManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func requestWhenInUse() {
+        guard manager.authorizationStatus == .notDetermined else { return }
+        manager.requestWhenInUseAuthorization()
+    }
+}
